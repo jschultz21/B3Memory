bc772b5f8d62beba67d3693582963e5b2a5c1cfc
Julia Schultz <julia.schultz91@gmail.com>
2016-06-08 15:28:31 -0400

    deck change option working

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var baseCard="";
 var secondCard;
 var paths=[];
 var score=0;
-var seconds = 0
-var minutes = 0
+var seconds=0;
+var minutes=0;
+var x=0;
+var deckChosen=false;
 
 //sets up the gameboard with sixteen divs
 for(i = 0; i < 16; i++) {
@@ -13,29 +15,35 @@ for(i = 0; i < 16; i++) {
   var hidden = $(".hidden");
 }
 
-//generates the deck with two copies of each image
-generateDeck(); //calls the generateDeck function
-// 
-// x = userinput
-// values for x:
-// bears = (0=x)
-// numbers = 1
-// letters = 2
+//lets user choose a deck before starting
+$(".deckChoice").on("click",function(){
+  deckChosen=true;
+  if ($(".card:contains('img')")) { //if the board contains images, clear them
+    $(".card").children("img").remove(); //removes all images from the .card divs
+    x = $(this).val(); //get value of deck clicked
+    generateDeck();
+  }
+  else { //otherwise, generate the deck
+    deckChosen=true;
+    x = $(this).val();
+    generateDeck();
+  }
 
+  //assigns an image to each div
+  $(".hidden").each(function() {
+    $(this).append(randomCard);
+  });
+});
+
+// //generates the deck with two copies of each image
 function generateDeck() {
   for(var i =1; i < 9; i++) {
-  var x=0;
     deck.push("images"+x+"/img"+ i + ".jpg");
     deck.push("images"+x+"/img"+ i + ".jpg");
   }
 }
 
-//assigns an image to each div
-$(".hidden").each(function() {
-  $(this).append(randomCard);
-});
-
-//appending a random image to each div
+// appending a random image to each div
 function randomCard () {
   var randomNumber = Math.floor(Math.random() * deck.length);
   var url = deck[randomNumber];
@@ -45,19 +53,27 @@ function randomCard () {
 };
 
 
+// when a hidden div is clicked, check if the board has images, and check if timer is NOT at 0
 hidden.on("click", function(){
-  if(!( seconds > 0 )) {
-    startTimer();
-  }
+  if ((deckChosen==true) && (seconds==0)) {
+      startTimer();
+    }
+    else{
+      console.log("timer not starting")
+    }
 });
 
 //when card is clicked, the background image is revealed
 hidden.on("click", revealCard);
 
-
 function revealCard() {
 
-  if ($(this).hasClass("hidden")){
+  if (deckChosen==false) {
+    alert("please choose a deck!");
+    return;
+  }
+
+   if ($(this).hasClass("hidden")){
 
     $(this).toggleClass("hidden selected"); //removes hidden class and adds selected class so image is revealed
     var path = $('img', this).attr('src'); //sets clicked img src to variable 'path'
@@ -90,43 +106,43 @@ function revealCard() {
 }
 
 
-  $(".reset").on("click", function(){
-    $(".card").children("img").remove(); //removes all images from the .card divs
-    deck=[]; //resets the deck
-    generateDeck(); //generates the deck
-    $(".card").addClass("hidden");  //resets all .card divs to hidden
-    $(".card").removeClass("selected matched");  //resets all .card divs to hidden
-    $(".card").append(randomCard); //assigns a random card to each div
-    score=0;
-    $("h2").html("Total Matches: "+score); //replace the scoreboard with new score
-    resetTimer();
-  });
+$(".reset").on("click", function(){
+  $(".card").children("img").remove(); //removes all images from the .card divs
+  deck=[]; //resets the deck
+  generateDeck(); //generates the deck
+  $(".card").addClass("hidden");  //resets all .card divs to hidden
+  $(".card").removeClass("selected matched");  //resets all .card divs to hidden
+  $(".card").append(randomCard); //assigns a random card to each div
+  score=0;
+  $("h2").html("Total Matches: "+score); //replace the scoreboard with new score
+  resetTimer();
+});
 
-  function doWeHaveaWinner() {
-    if (score === 8){
-      alert("U WIN GR8 JB!");
-    }
-    else{
-      console.log("no winner yet");
-    }
+function doWeHaveaWinner() {
+  if (score === 8){
+    alert("U WIN GR8 JB!");
   }
-
-  function startTimer(){
-    timer=setInterval(updateTimer, 1000);
+  else{
+    console.log("no winner yet");
   }
+}
 
-  function updateTimer() {
-    seconds ++;
-    if (seconds >=60) {
-      seconds=0;
-      minutes ++
-    }
-    $("#timer").text(minutes+"m "+seconds+"s");
-  }
+function startTimer(){
+  timer=setInterval(updateTimer, 1000);
+}
 
-  function resetTimer(){
-    clearInterval(timer);
+function updateTimer() {
+  seconds ++;
+  if (seconds >=60) {
     seconds=0;
-    $("#timer").text("0m 0s");
-    timing=false;
+    minutes ++
+  }
+  $("#timer").text(minutes+"m "+seconds+"s");
+}
+
+function resetTimer(){
+  clearInterval(timer);
+  seconds=0;
+  $("#timer").text("0m 0s");
+  timing=false;
 }
