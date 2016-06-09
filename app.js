734b4103915914c9006f84bbf95c4e9ca314f065
Julia Schultz <julia.schultz91@gmail.com>
2016-06-09 17:42:02 -0400

    fixing deck change

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $("#bears").click();
+  $("#bears").click();
 });
 
 
@@ -20,10 +20,6 @@ for(i = 0; i < 16; i++) {
   $(".gameboard").append('<div class="hidden card"/>');
   var hidden = $(".hidden");
 }
-
-
-
-
 
 //lets user choose a deck before starting
 $(".deckChoice").on("click",function(){
@@ -74,73 +70,56 @@ function randomCard () {
 hidden.on("click", function(){
   if ((deckChosen==true) && (timing==false)) {
     startTimer();  }
-  else{
-  }
-});
+    else{
+    }
+  });
 
-//when card is clicked, the background image is revealed
-hidden.on("click", revealCard);
+  //when card is clicked, the background image is revealed
+  hidden.on("click", revealCard);
 
-function revealCard() {
+  function revealCard() {
 
-  if (deckChosen==false) {
-    alert("please choose a deck!");
-    return;
-  }
-
-  if ($(this).hasClass("hidden")&&(canClickCard==true)){
-
-    $(this).toggleClass("hidden selected"); //removes hidden class and adds selected class so image is revealed
-    var path = $('img', this).attr('src'); //sets clicked img src to variable 'path'
-    paths.push(path); //pushes path variable into paths array
-
-    baseCard=paths[paths.length-2];
-    secondCard=paths[paths.length-1];
-
-    if (typeof(baseCard) === 'undefined') { //if there is no basecard yet, do nothing after first card is selected
+    if (deckChosen==false) {
+      alert("please choose a deck!");
+      return;
     }
 
-    else { //if there IS a baseCard, do these things:
-      if (baseCard === secondCard) { //if the second selected card and the base card match, do these things:
-        console.log("hooray!");
-        $(this).toggleClass("selected matched");
-        $('img[src="' + baseCard + '"]').parent(".selected").toggleClass("matched selected");
-        score ++ //add one to the score
-        $("h2").html("Total Matches: "+score); //replace the scoreboard with new score
-        doWeHaveaWinner(); //check for winner
+    if ($(this).hasClass("hidden")&&(canClickCard==true)){
+
+      $(this).toggleClass("hidden selected"); //removes hidden class and adds selected class so image is revealed
+      var path = $('img', this).attr('src'); //sets clicked img src to variable 'path'
+      paths.push(path); //pushes path variable into paths array
+
+      baseCard=paths[paths.length-2];
+      secondCard=paths[paths.length-1];
+
+      if (typeof(baseCard) === 'undefined') { //if there is no basecard yet, do nothing after first card is selected
       }
 
-      else { //if the second selected card and base card do not match, do these things:
-        canClickCard=false;
-        setTimeout(function(){
-          $('img[src="' + secondCard + '"]').parent(".selected").toggleClass("hidden selected"); //adds the hidden class and removes selected classso the second selected card becomes hidden again
-          $('img[src="' + baseCard + '"]').parent(".selected").toggleClass("hidden selected"); //adds the hidden class so the base card becomes hidden again
-          canClickCard=true;
-        }, 600);
+      else { //if there IS a baseCard, do these things:
+        if (baseCard === secondCard) { //if the second selected card and the base card match, do these things:
+          console.log("hooray!");
+          $(this).toggleClass("selected matched");
+          $('img[src="' + baseCard + '"]').parent(".selected").toggleClass("matched selected");
+          score ++ //add one to the score
+          $("h2").html("Total Matches: "+score); //replace the scoreboard with new score
+          doWeHaveaWinner(); //check for winner
+        }
+
+        else { //if the second selected card and base card do not match, do these things:
+          canClickCard=false;
+          setTimeout(function(){
+            $('img[src="' + secondCard + '"]').parent(".selected").toggleClass("hidden selected"); //adds the hidden class and removes selected classso the second selected card becomes hidden again
+            $('img[src="' + baseCard + '"]').parent(".selected").toggleClass("hidden selected"); //adds the hidden class so the base card becomes hidden again
+            canClickCard=true;
+          }, 600);
+        }
+        paths=[]; //resets the array
       }
-      paths=[]; //resets the array
     }
   }
-}
 
-$("#reset").on("click", function(){
-  $(".card").children("img").remove(); //removes all images from the .card divs
-  generateDeck(); //generates the deck
-  $(".card").addClass("hidden");  //resets all .card divs to hidden
-  $(".card").removeClass("selected matched");  //resets all .card divs to hidden
-  $(".card").append(randomCard); //assigns a random card to each div
-  score=0;
-  $("h2").html("Total Matches: "+score); //replace the scoreboard with new score
-  timing=false;
-  seconds=0;
-  resetTimer();
-});
-
-function doWeHaveaWinner() {
-  if (score === 8){
-    alert("#WINNING");
-
-
+  $("#reset").on("click", function(){
     $(".card").children("img").remove(); //removes all images from the .card divs
     generateDeck(); //generates the deck
     $(".card").addClass("hidden");  //resets all .card divs to hidden
@@ -148,36 +127,50 @@ function doWeHaveaWinner() {
     $(".card").append(randomCard); //assigns a random card to each div
     score=0;
     $("h2").html("Total Matches: "+score); //replace the scoreboard with new score
-
     timing=false;
     resetTimer();
-  }
-  else{
-    console.log("no winner yet");
-  }
-}
+  });
 
-function startTimer(){
-  timing=true;
-  timer=setInterval(updateTimer, 1000);
-}
+  function doWeHaveaWinner() {
+    if (score === 8){
+      alert("#WINNING");
+      $(".card").children("img").remove(); //removes all images from the .card divs
+      generateDeck(); //generates the deck
+      $(".card").addClass("hidden");  //resets all .card divs to hidden
+      $(".card").removeClass("selected matched");  //resets all .card divs to hidden
+      $(".card").append(randomCard); //assigns a random card to each div
+      score=0;
+      $("h2").html("Total Matches: "+score); //replace the scoreboard with new score
+      timing=false;
+      resetTimer();
+      deckChosen=true;
+    }
+    else{
+      console.log("no winner yet");
+    }
+  }
 
-function updateTimer() {
-  seconds ++;
-  if (seconds >=60) {
+  function startTimer(){
+    timing=true;
+    timer=setInterval(updateTimer, 1000);
+  }
+
+  function updateTimer() {
+    seconds ++;
+    if (seconds >=60) {
+      seconds=0;
+      minutes ++
+    }
+    $("#timer").text(minutes+"m "+seconds+"s");
+  }
+
+  function resetTimer(){
+    clearInterval(timer);
     seconds=0;
-    minutes ++
+    $("#timer").text("0m 0s");
   }
-  $("#timer").text(minutes+"m "+seconds+"s");
-}
 
-function resetTimer(){
-  clearInterval(timer);
-  seconds=0;
-  $("#timer").text("0m 0s");
-}
-
-//prevents image drag CHEATERS!!
-$("img").mousedown(function(e){
-  e.preventDefault()
-});
+  //prevents image drag CHEATERS!!
+  $("img").mousedown(function(e){
+    e.preventDefault()
+  });
